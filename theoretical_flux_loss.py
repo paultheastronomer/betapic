c180874ef0f9e0c1982fb325be0ce2646d5e415e
Dr. Paul Anthony Wilson <paultheastronomer@protonmail.com>
2016-02-07 09:58:35 +0100

    plot theoretical flux

--- theoretical_flux_loss.py
+++ theoretical_flux_loss.py
@@ -2,18 +2,19 @@ from scipy.integrate import quad
 import numpy as np
 import matplotlib.pyplot as plt
 
-def integrand_theta(x, rho,R,sigma):    
-    return np.exp(2*rho*R*np.cos(x) / 2*sigma**2)
+def integrand_theta(theta, rho,R,sigma):	# Integration with respect to theta
+    return np.exp(2*rho*R*np.cos(theta) / 2*sigma**2)
 
-def integrand_R(y,rho,sigma):    
-    return y*np.exp(-y**2 / 2*sigma**2)*quad(integrand_theta, 0, 2*np.pi, args=(rho,y,sigma))[0]*np.exp(-rho**2 / 2*sigma**2)
+def integrand_R(R,rho,sigma):			# Integration with respect to R  
+    return R*np.exp(-R**2 / 2*sigma**2)*quad(integrand_theta, 0, 2*np.pi, args=(rho,R,sigma))[0]*np.exp(-rho**2 / 2*sigma**2)
 
 def main():
 
-	R       = 1.25      # size of aperture. R = Diameter of 2.5"/2 = 1.25"
-	FWHM    = 0.715     # FWHM in arcseconds: 6.5 pix * 0.11"/pix = 0.715"
-	sigma   = 2*np.sqrt(2*np.log(2))
-	rho     = np.arange(0,2.6,0.1)
+	FWHM		= 1.7	# FWHM in Cross dispersion direction p.56 COS manual
+	R       	= 1.25	# Aperture size. R = Diameter of 2.5"/2 = 1.25"
+	
+	sigma   	= FWHM/ 2*np.sqrt(2*np.log(2))	# Convert from FWHM --> sigma
+	rho     	= np.arange(0,1.5,0.1)			# [arcsec]
 
 	flux	= []
 	shift	= []
@@ -21,9 +22,9 @@ def main():
 	for i in range(len(rho)):
 		flux.append(quad(integrand_R, 0, R, args=(rho[i],sigma))[0])
 		shift.append(rho[i])
-
 	
 	flux = np.array(flux)
+	shift = np.array(shift)
 	
 	plt.plot(shift,flux/flux[0],'-k')
 	plt.ylabel('Flux loss')
