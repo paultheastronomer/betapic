50b67d1391929dc2c71239f0aaaf8f8f7dc5e782
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-02-26 18:21:18 +0100

    adjusted cut-offs

--- Ly_data.py
+++ Ly_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import factorial
 
 def CF(flux,flux_err,ref,ref_err,n1,n2):
     ratio = np.average(flux[n1:n2], axis=0, weights=1./(flux_err[n1:n2]**2))/ \
@@ -14,6 +15,81 @@ def weighted_avg_and_errorbars(Flux, Err):
     average = np.average(Flux, axis=0, weights=weights)
     errorbars_2 = np.sum((weights*Err)**2, axis=0)/ ((np.sum(weights, axis=0))**2)
     return average, np.sqrt(errorbars_2)
+
+#https://scipy.github.io/old-wiki/pages/Cookbook/SavitzkyGolay
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    
+    Example: #c0 = savitzky_golay(f0_bin_corr, 7, 3)
+    
+    """
+
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError, msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
+
 
 def main():    
     #############################################################################################
@@ -38,8 +114,8 @@ def main():
 
     # Choose a region to normalise the spectra
     # units refer to array elements. 450 = start at the 451st element 
-    n1 = 450
-    n2 = 800
+    n1 = 800#450
+    n2 = 950#800
     
     # Calculate the Correction Factor 
     C   = [CF(F0_1,E0_1,F0_0,E0_0,n1,n2),CF(F1_1,E1_1,F0_0,E0_0,n1,n2),CF(F2_1,E2_1,F0_0,E0_0,n1,n2),\
@@ -97,16 +173,46 @@ def main():
     #Err  = np.array([Ec[0],Ec[1],Ec[2],Ec[3],Ec[4],Ec[5],Ec[6],Ec[7]])
     F_tot, F_tot_err    =  weighted_avg_and_errorbars(Flux,Err)
     #############################################################################################
+
+
     
     # Decide at which RV airglow affects the different measurements
     # Units in km/s
-    shift_0_l       = -235#337.5
-    shift_0_r       =  220#271.7
-    shift_08_l      = shift_0_l+68#-183.3
-    shift_08_r      = shift_0_r-68#157#142.5
-    shift_11_r      = shift_0_r-94# 106
+    shift_0_l       = -350#337.5
+    shift_0_r       =  295#315#271.7
+    shift_08_l      = -165#shift_0_l+68#-183.3
+    shift_08_r      = 140#143#shift_0_r-68#157#142.5
+    shift_11_r      = 108#113#shift_0_r-94# 106
+
+    ''' This code if good for finding the RV limits above
+    Flux = np.array([Fc[0],Fc[1],Fc[2]])
+    Err  = np.array([Ec[0],Ec[1],Ec[2]])
+    F_v1, E_v1    =  weighted_avg_and_errorbars(Flux,Err)
+
+    Flux = np.array([Fc[3],Fc[4],Fc[5],Fc[6]])
+    Err  = np.array([Ec[3],Ec[4],Ec[5],Ec[6]])
+    F_v2, E_v2    =  weighted_avg_and_errorbars(Flux,Err)
+    
+    Flux = np.array([Fc[7],Fc[8],Fc[9],Fc[10]])
+    Err  = np.array([Ec[7],Ec[8],Ec[9],Ec[10]])
+    F_v3, E_v3    =  weighted_avg_and_errorbars(Flux,Err)
+
+    pnts = 7
+    order=3
+    
+    c0 = savitzky_golay(F0_0, pnts, order)
+    c1 = savitzky_golay(F1, pnts, order)
+    c2 = savitzky_golay(F2, pnts, order)
+    c3 = savitzky_golay(F3, pnts, order)
 
 
+    plt.step(RV,c0,color='black')
+    plt.step(RV,c1)
+    plt.step(RV,c2)
+    plt.step(RV,c3)
+
+    '''
+    #'''
     #plt.plot(RV,AG0)
     plt.errorbar(RV,F_tot,yerr=F_tot_err,fmt='',ecolor='black')
     plt.plot(RV,F_tot, marker='o', color='k')
@@ -121,7 +227,8 @@ def main():
         
         # Save 0.8" shift data
         if  shift_0_l < RV[j] < shift_08_l:
-            plt.errorbar(RV[j],F1[j],yerr=F1_err[j],color='blue')
+            #plt.errorbar(RV[j],F1[j],yerr=F1_err[j],color='blue')
+            plt.plot(RV[j],F1[j], marker='o', color='b')
             print >> f, " ","{: 1.10e}".format(W[j])," "+"{: 1.10e}".format(F1[j])," "+"{: 1.10e}".format(F1_err[j])
         if  shift_08_r < RV[j] < shift_0_r:
             #plt.errorbar(RV[j],F2[j],yerr=F2_err[j],fmt='',color='blue')
@@ -137,10 +244,10 @@ def main():
     # Plot the region used for normalisation
     plt.step(RV[n1:n2],F_tot[n1:n2],color='green')
 
+    #'''
     plt.xlabel(r'RV (km/s)')
     plt.ylabel('Flux')
     plt.xlim(-500,500)  
-    #plt.xlim(-2000,800)
     plt.ylim(-0.3e-14,5.5e-14)
     plt.show()
 
