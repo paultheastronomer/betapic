8421d84c5b3355ec7ef69d428517ac65fb03ba92
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-04-20 13:59:33 +0200

    update

--- Ly_data.py
+++ Ly_data.py
@@ -16,81 +16,6 @@ def weighted_avg_and_errorbars(Flux, Err):
     errorbars_2 = np.sum((weights*Err)**2, axis=0)/ ((np.sum(weights, axis=0))**2)
     return average, np.sqrt(errorbars_2)
 
-#https://scipy.github.io/old-wiki/pages/Cookbook/SavitzkyGolay
-def savitzky_golay(y, window_size, order, deriv=0, rate=1):
-    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-    The Savitzky-Golay filter removes high frequency noise from data.
-    It has the advantage of preserving the original shape and
-    features of the signal better than other types of filtering
-    approaches, such as moving averages techniques.
-    Parameters
-    ----------
-    y : array_like, shape (N,)
-        the values of the time history of the signal.
-    window_size : int
-        the length of the window. Must be an odd integer number.
-    order : int
-        the order of the polynomial used in the filtering.
-        Must be less then `window_size` - 1.
-    deriv: int
-        the order of the derivative to compute (default = 0 means only smoothing)
-    Returns
-    -------
-    ys : ndarray, shape (N)
-        the smoothed signal (or it's n-th derivative).
-    Notes
-    -----
-    The Savitzky-Golay is a type of low-pass filter, particularly
-    suited for smoothing noisy data. The main idea behind this
-    approach is to make for each point a least-square fit with a
-    polynomial of high order over a odd-sized window centered at
-    the point.
-    Examples
-    --------
-    t = np.linspace(-4, 4, 500)
-    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-    ysg = savitzky_golay(y, window_size=31, order=4)
-    import matplotlib.pyplot as plt
-    plt.plot(t, y, label='Noisy signal')
-    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-    plt.plot(t, ysg, 'r', label='Filtered signal')
-    plt.legend()
-    plt.show()
-    References
-    ----------
-    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-       Data by Simplified Least Squares Procedures. Analytical
-       Chemistry, 1964, 36 (8), pp 1627-1639.
-    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-       Cambridge University Press ISBN-13: 9780521880688
-    
-    Example: #c0 = savitzky_golay(f0_bin_corr, 7, 3)
-    
-    """
-
-    try:
-        window_size = np.abs(np.int(window_size))
-        order = np.abs(np.int(order))
-    except ValueError, msg:
-        raise ValueError("window_size and order have to be of type int")
-    if window_size % 2 != 1 or window_size < 1:
-        raise TypeError("window_size size must be a positive odd number")
-    if window_size < order + 2:
-        raise TypeError("window_size is too small for the polynomials order")
-    order_range = range(order+1)
-    half_window = (window_size -1) // 2
-    # precompute coefficients
-    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-    # pad the signal at the extremes with
-    # values taken from the signal itself
-    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-    y = np.concatenate((firstvals, y, lastvals))
-    return np.convolve( m[::-1], y, mode='valid')
-
-
 def main():    
     #############################################################################################
     #
@@ -133,45 +58,104 @@ def main():
 
     # -0.8" Ly-alpha wing
     #############################################################################################    
+    # For all data uncomment the two lines below
     Flux = np.array([Fc[1],Fc[4],Fc[8]])
     Err  = np.array([Ec[1],Ec[4],Ec[8]])
+    
+    # For 10 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[1]])
+    #Err  = np.array([Ec[1]])
+
+    # For 24 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[4]])
+    #Err  = np.array([Ec[4]])
+
+    # For 30 Jan data uncomment the two lines below
+    #Flux = np.array([Fc[8]])
+    #Err  = np.array([Ec[8]])
+    
     F1, F1_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################    
 
     # 0.8" Ly-alpha wing
     #############################################################################################
+    # For all data uncomment the two lines below
     Flux = np.array([Fc[2],Fc[5],Fc[9]])
     Err  = np.array([Ec[2],Ec[5],Ec[9]])
-    #Flux = np.array([Fc[2],Fc[5]])
-    #Err  = np.array([Ec[2],Ec[5]])
+    
+    # For 10 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[2]])
+    #Err  = np.array([Ec[2]])
+
+    # For 24 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[5]])
+    #Err  = np.array([Ec[5]])
+
+    # For 30 Jan data uncomment the two lines below
+    #Flux = np.array([Fc[9]])
+    #Err  = np.array([Ec[9]])
+
     F2, F2_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
 
     # 1.1" Ly-alpha wing
     #############################################################################################
+    # For all data uncomment the two lines below
     Flux = np.array([Fc[6],Fc[10]])
     Err  = np.array([Ec[6],Ec[10]])
+
+    # For 24 Dec data uncomment the two lines below    
     #Flux = np.array([Fc[6]])
     #Err  = np.array([Ec[6]])
+
+    # For 30 Jan data uncomment the two lines below    
+    #Flux = np.array([Fc[10]])
+    #Err  = np.array([Ec[10]])
+    
     F3, F3_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
 
     # Combining 0.8" and 1.1" data of the Ly-alpha wing
     #############################################################################################
+    # For all data uncomment the two lines below
     Flux = np.array([Fc[2],Fc[5],Fc[9],Fc[6],Fc[10]])
     Err  = np.array([Ec[2],Ec[5],Ec[9],Ec[6],Ec[10]])
-    #Flux = np.array([Fc[2],Fc[5],Fc[6]])
-    #Err  = np.array([Ec[2],Ec[5],Ec[6]])
+    
+    # For 10 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[2]])
+    #Err  = np.array([Ec[2]])
+
+    # For 24 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[5],Fc[6]])
+    #Err  = np.array([Ec[5],Ec[6]])
+
+    # For 30 Jan data uncomment the two lines below
+    #Flux = np.array([Fc[9],Fc[10]])
+    #Err  = np.array([Ec[9],Ec[10]])
+    
     F4, F4_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
 
     # Combine all data regardless of shift
     #############################################################################################
+    # For all data uncomment the two lines below
     Flux = np.array([F0_0,Fc[0],Fc[1],Fc[2],Fc[3],Fc[4],Fc[5],Fc[6],Fc[7],Fc[8],Fc[9],Fc[10]])
     Err  = np.array([E0_0,Ec[0],Ec[1],Ec[2],Ec[3],Ec[4],Ec[5],Ec[6],Ec[7],Ec[8],Ec[9],Ec[10]])
-    #Flux = np.array([Fc[0],Fc[1],Fc[2],Fc[3],Fc[4],Fc[5],Fc[6],Fc[7]])
-    #Err  = np.array([Ec[0],Ec[1],Ec[2],Ec[3],Ec[4],Ec[5],Ec[6],Ec[7]])
-    F_tot, F_tot_err    =  weighted_avg_and_errorbars(Flux,Err)
+    
+    # For 10 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[0],Fc[1],Fc[2]])
+    #Err  = np.array([Ec[0],Ec[1],Ec[2]])
+
+    # For 24 Dec data uncomment the two lines below
+    #Flux = np.array([Fc[3],Fc[4],Fc[5],Fc[6]])
+    #Err  = np.array([Ec[3],Ec[4],Ec[5],Ec[6]])
+
+    # For 30 Jan data uncomment the two lines below
+    #Flux = np.array([Fc[7],Fc[8],Fc[9],Fc[10]])
+    #Err  = np.array([Ec[7],Ec[8],Ec[9],Ec[10]])
+    
+    #F_tot, F_tot_err    =  weighted_avg_and_errorbars(Flux,Err)
+    F_tot, F_tot_err    =  F0_0, E0_0
     #############################################################################################
 
 
@@ -224,7 +208,6 @@ def main():
             print >> f, " ","{: 1.10e}".format(W[j])," "+"{: 1.10e}".format(F_tot[j])," "+"{: 1.10e}".format(F_tot_err[j])       
         if RV[j] > shift_0_r:
             print >> f, " ","{: 1.10e}".format(W[j])," "+"{: 1.10e}".format(F_tot[j])," "+"{: 1.10e}".format(F_tot_err[j])
-        
         # Save 0.8" shift data
         if  shift_0_l < RV[j] < shift_08_l:
             #plt.errorbar(RV[j],F1[j],yerr=F1_err[j],color='blue')
