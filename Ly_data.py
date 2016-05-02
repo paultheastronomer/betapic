627df8c29a2bef3e4b252491d094ac1c32a26a23
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-05-02 10:39:38 +0200

    old_version

--- Ly_data.py
+++ Ly_data.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial
 
-def CF(flux,flux_err,ref,ref_err,n1,n2):
-    ratio = np.average(flux[n1:n2], axis=0, weights=1./(flux_err[n1:n2]**2))/ \
-            np.average(ref[n1:n2],  axis=0, weights=1./(ref_err[n1:n2]**2 ))                       
-    print 1./ratio
-    return 1./ratio
+def Bin_data(x,y1,e1,bin_pnts):
+    bin_size    = int(len(x)/bin_pnts)
+    bins        = np.linspace(x[0], x[-1], bin_size)
+    digitized   = np.digitize(x, bins)
+    bin_y       = np.array([y1[digitized == i].mean() for i in range(0, len(bins))])
+    bin_e       = np.array([e1[digitized == i].mean() for i in range(0, len(bins))])
+    return bins, bin_y, bin_e/np.sqrt(bin_pnts)
 
 
 def weighted_avg_and_errorbars(Flux, Err):
@@ -17,6 +19,14 @@ def weighted_avg_and_errorbars(Flux, Err):
     average = np.average(Flux, axis=0, weights=weights)
     errorbars_2 = np.sum(weights*(Err**2), axis=0) / np.sum(weights, axis=0)
     return average, np.sqrt(errorbars_2)
+
+def ShiftAG(AG,units):
+    zeros   = np.zeros(abs(units))
+    if units > 0.:
+        AG      = np.concatenate((zeros,AG), axis=1)[:-units]
+    else:
+        AG      = np.concatenate((AG,zeros), axis=1)[abs(units):]
+    return AG
 
 def main():    
     #############################################################################################
@@ -39,26 +49,11 @@ def main():
     W, RV, F0_3, E0_3, F1_3, E1_3, F2_3, E2_3, F3_3, E3_3, AG3, AG3err, F_ave_w_3 = np.genfromtxt('/home/paw/science/betapic/data/HST/dat/B_30Jan.dat',unpack=True,skiprows=7000,skip_footer=6000)
     ############################################################################################# 
 
-    # Choose a region to normalise the spectra
-    # units refer to array elements. 450 = start at the 451st element 
-    n1 = 800#450
-    n2 = 950#800
-    
-    # Calculate the Correction Factor 
-    C   = [CF(F0_1,E0_1,F0_0,E0_0,n1,n2),CF(F1_1,E1_1,F0_0,E0_0,n1,n2),CF(F2_1,E2_1,F0_0,E0_0,n1,n2),\
-    CF(F0_2,E0_2,F0_0,E0_0,n1,n2),CF(F1_2,E1_2,F0_0,E0_0,n1,n2),CF(F2_2,E2_2,F0_0,E0_0,n1,n2),CF(F3_2,E3_2,F0_0,E0_0,n1,n2),\
-    CF(F0_3,E0_3,F0_0,E0_0,n1,n2),CF(F1_3,E1_3,F0_0,E0_0,n1,n2),CF(F2_3,E2_3,F0_0,E0_0,n1,n2),CF(F3_3,E3_3,F0_0,E0_0,n1,n2)]
-    
+    dat_directory = "/home/paw/science/betapic/data/HST/dat/"
+
     Fc  = [F0_1,F1_1,F2_1,F0_2,F1_2,F2_2,F3_2,F0_3,F1_3,F2_3,F3_3]
     Ec  = [E0_1,E1_1,E2_1,E0_2,E1_2,E2_2,E3_2,E0_3,E1_3,E2_3,E3_3]
-    '''
-    Fc = [[] for _ in range(len(C))]
-    Ec = [[] for _ in range(len(C))]
     
-    for i in range(len(C)):
-        Fc[i] = F[i]*C[i]   # Correct for lower efficiency
-        Ec[i] = E[i]*C[i]   # accordingly correct the tabulated error bars
-    '''
     # -0.8" Ly-alpha wing
     #############################################################################################    
     # For all data uncomment the two lines below
@@ -74,8 +69,8 @@ def main():
     #Err  = np.array([Ec[4]])
 
     # For 30 Jan data uncomment the two lines below
-    #Flux = np.array([Fc[8]])
-    #Err  = np.array([Ec[8]])
+    Flux = np.array([Fc[8]])
+    Err  = np.array([Ec[8]])
     
     F1, F1_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################    
@@ -95,8 +90,8 @@ def main():
     #Err  = np.array([Ec[5]])
 
     # For 30 Jan data uncomment the two lines below
-    #Flux = np.array([Fc[9]])
-    #Err  = np.array([Ec[9]])
+    Flux = np.array([Fc[9]])
+    Err  = np.array([Ec[9]])
 
     F2, F2_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
@@ -112,8 +107,8 @@ def main():
     #Err  = np.array([Ec[6]])
 
     # For 30 Jan data uncomment the two lines below    
-    #Flux = np.array([Fc[10]])
-    #Err  = np.array([Ec[10]])
+    Flux = np.array([Fc[10]])
+    Err  = np.array([Ec[10]])
     
     F3, F3_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
@@ -133,8 +128,8 @@ def main():
     #Err  = np.array([Ec[5],Ec[6]])
 
     # For 30 Jan data uncomment the two lines below
-    #Flux = np.array([Fc[9],Fc[10]])
-    #Err  = np.array([Ec[9],Ec[10]])
+    Flux = np.array([Fc[9],Fc[10]])
+    Err  = np.array([Ec[9],Ec[10]])
     
     F4, F4_err    =  weighted_avg_and_errorbars(Flux,Err)         
     #############################################################################################
@@ -142,55 +137,74 @@ def main():
     # Combine all data regardless of shift
     #############################################################################################
     # For all data uncomment the two lines below
-    Flux = np.array([F0_0,Fc[0],Fc[1],Fc[2],Fc[3],Fc[4],Fc[5],Fc[6],Fc[7],Fc[8],Fc[9],Fc[10]])
-    Err  = np.array([E0_0,Ec[0],Ec[1],Ec[2],Ec[3],Ec[4],Ec[5],Ec[6],Ec[7],Ec[8],Ec[9],Ec[10]])
+    #Flux = np.array([F0_0,Fc[0],Fc[1],Fc[2],Fc[3],Fc[4],Fc[5],Fc[6],Fc[7],Fc[8],Fc[9],Fc[10]])
+    #Err  = np.array([E0_0,Ec[0],Ec[1],Ec[2],Ec[3],Ec[4],Ec[5],Ec[6],Ec[7],Ec[8],Ec[9],Ec[10]])
+
+    '''
+    AirG                = np.array([AG0,AG1,AG3])
+    AirG_err            = np.array([AG0err,AG1err,AG3err])
+    AirG_W, AirG_W_err  = weighted_avg_and_errorbars(AirG,AirG_err)    
+    
+    AG1    = ShiftAG(AirG_W,-28)   #2015v1 +0.8" AG
+    AG2    = ShiftAG(AirG_W,-32)   #2015v2 +0.8" AG
+    AG3    = ShiftAG(AirG_W,-25)   #2016 +0.8" AG
+
+    AG4    = ShiftAG(AirG_W,-42)   #2015v2 +1.1" AG
+    AG5    = ShiftAG(AirG_W,-43)   #2016 +1.1" AG
+
+    AG6    = ShiftAG(AirG_W,32)   #2015v1 -0.8" AG
+    AG7    = ShiftAG(AirG_W,35)   #2015v2 -0.8" AG
+    AG8    = ShiftAG(AirG_W,33)   #2016 -0.8" AG
+    
+    #plt.plot(RV,AG1,color="blue")
+    #plt.plot(RV,AG2,color="red")
+    #plt.plot(RV,AG3,color="green")
+    
+    #plt.plot(RV,AG4,color="orange")
+    #plt.plot(RV,AG5,color="purple")
+
+    plt.plot(RV,AG6,color="blue")
+    plt.plot(RV,AG7,color="red")
+    plt.plot(RV,AG8,color="green")
+    
+    plt.plot(RV,AirG_W,color="purple")
+    
+    #plt.errorbar(W[start:stop],F0_0[start:stop],yerr=E0_0[start:stop],color="red")
+    #plt.xlim(50,500)
+    plt.xlim(-500,50)
+    plt.ylim(0,6e-14)
+    plt.show()
+    sys.exit()
+    '''
+     
+    # For 2014 data see line straight after "F_tot, F_tot_err"
     
     # For 10 Dec data uncomment the two lines below
-    #Flux = np.array([Fc[0],Fc[1],Fc[2]])
-    #Err  = np.array([Ec[0],Ec[1],Ec[2]])
+    Flux = np.array([Fc[0],Fc[1],Fc[2]])
+    Err  = np.array([Ec[0],Ec[1],Ec[2]])
 
     # For 24 Dec data uncomment the two lines below
     #Flux = np.array([Fc[3],Fc[4],Fc[5],Fc[6]])
     #Err  = np.array([Ec[3],Ec[4],Ec[5],Ec[6]])
 
     # For 30 Jan data uncomment the two lines below
-    #Flux = np.array([Fc[7],Fc[8],Fc[9],Fc[10]])
-    #Err  = np.array([Ec[7],Ec[8],Ec[9],Ec[10]])
+    Flux = np.array([Fc[7],Fc[8],Fc[9],Fc[10]])
+    Err  = np.array([Ec[7],Ec[8],Ec[9],Ec[10]])
     
     F_tot, F_tot_err    =  weighted_avg_and_errorbars(Flux,Err)
     #F_tot, F_tot_err    =  F0_0, E0_0
     #############################################################################################
 
 
-    
     # Decide at which RV airglow affects the different measurements
     # Units in km/s
-    shift_0_l       = -350#337.5
-    shift_0_r       =  295#315#271.7
-    shift_08_l      = -165#shift_0_l+68#-183.3
-    shift_08_r      = 140#143#shift_0_r-68#157#142.5
-    shift_11_r      = 108#113#shift_0_r-94# 106
-
-    ''' This code if good for finding the RV limits above
-    Flux = np.array([Fc[0],Fc[1],Fc[2]])
-    Err  = np.array([Ec[0],Ec[1],Ec[2]])
-    F_v1, E_v1    =  weighted_avg_and_errorbars(Flux,Err)
-
-    Flux = np.array([Fc[3],Fc[4],Fc[5],Fc[6]])
-    Err  = np.array([Ec[3],Ec[4],Ec[5],Ec[6]])
-    F_v2, E_v2    =  weighted_avg_and_errorbars(Flux,Err)
+    shift_0_l       = -365#337.5 350
+    shift_0_r       =  295#315#271.7 295
+    shift_08_l      = -164#shift_0_l+68#-183.3  ok
+    shift_08_r      = 135#143#shift_0_r-68#157#142.5 140!
+    shift_11_r      = 115#115#113#shift_0_r-94# 108 ok
     
-    Flux = np.array([Fc[7],Fc[8],Fc[9],Fc[10]])
-    Err  = np.array([Ec[7],Ec[8],Ec[9],Ec[10]])
-    F_v3, E_v3    =  weighted_avg_and_errorbars(Flux,Err)
-    '''
-    #'''
-    #plt.plot(RV,AG0)
-    plt.errorbar(RV,F_tot,yerr=F_tot_err,fmt='',ecolor='black')
-    plt.plot(RV,F_tot, marker='o', color='k')
-    # Plot the spectra using the cuts above and write to .dat file.
-    #'''
-    f = open('Ly-alpha_no_CF.dat', 'w+')
+    f = open(dat_directory+'Ly-alpha_B_30Jan.dat', 'w+')
     for j in range(len(RV)):
         # Save 0.0" shift data
         if RV[j] < shift_0_l:
@@ -207,10 +221,12 @@ def main():
             plt.plot(RV[j],F2[j], marker='o', color='b')
             print >> f, " ","{: 1.10e}".format(W[j])," "+"{: 1.10e}".format(F2[j])," "+"{: 1.10e}".format(F2_err[j])
         # Save 1.1" shift data
+        #'''
         if  shift_11_r < RV[j] < shift_08_r:
             #plt.errorbar(RV[j],F3[j],yerr=F3_err[j],fmt='',color='red')
             plt.plot(RV[j],F3[j], marker='o', color='r')
             print >> f, " ","{: 1.10e}".format(W[j])," "+"{: 1.10e}".format(F3[j])," "+"{: 1.10e}".format(F3_err[j])
+        #'''
     f.close()
     #'''
     
@@ -221,7 +237,7 @@ def main():
     plt.xlabel(r'RV (km/s)')
     plt.ylabel('Flux')
     plt.xlim(-500,500)  
-    plt.ylim(-0.3e-14,5.5e-14)
+    #splt.ylim(-0.3e-14,5.5e-14)
     plt.show()
 
 if __name__ == '__main__':
