734c703e825edf910ed4e04678223d1a56312669
Paul A. Wilson <paultheastronomer@users.noreply.github.com>
2016-05-24 14:56:14 +0200

    slight changes

--- Ly_fit.py
+++ Ly_fit.py
@@ -240,8 +240,8 @@ def main():
 
     dat_directory   = "/home/paw/science/betapic/data/HST/dat/"
 
-    Wo, Fo, Eo      = np.genfromtxt(dat_directory+'Ly_sky_subtracted_new.txt',unpack=True)
-    W, F, E         = np.genfromtxt(dat_directory+'Ly_sky_subtracted_new.txt',unpack=True,skip_footer= 600)
+    Wo, Fo, Eo      = np.genfromtxt(dat_directory+'Ly_sky_subtracted.txt',unpack=True)
+    W, F, E     = np.genfromtxt(dat_directory+'Ly_sky_subtracted.txt',unpack=True,skip_footer= 640)
     
 
     #np.savetxt(dat_directory+"Ly_a_20160511_v2.txt",np.column_stack((Wo, Fo, Eo)))
@@ -264,21 +264,21 @@ def main():
 
     # Beta Pic parameters
     v_bp            = 20.5        # RV of the beta Pic (relative to Heliocentric)
-    nh_bp           = 19.20       # Column density beta Pic, Fitting param
+    nh_bp           = 18.87       # Column density beta Pic, Fitting param
     b_bp            = 4.0        # Turbulent velocity
     T_bp            = 1000.       # Temperture of gas in beta Pic disk
 
     # Extra component parameters
-    v_X             = 90.0        # RV of the beta Pic (relative to Heliocentric)
-    nh_X            = 18.34       # Column density beta Pic, Fitting param
+    v_X             = 34.0        # RV of the beta Pic (relative to Heliocentric)
+    nh_X            = 19.25       # Column density beta Pic, Fitting param
     b_X             = 6.0        # Turbulent velocity
     T_X             = 1000.       # Temperture of gas in beta Pic disk
 
     # Stellar emission line parameters
-    max_f           = 10.2e-12     # Fitting param                 
+    max_f           = 1.7e-11     # Fitting param                 
     dp              = 0.0 
-    uf              = 3.12#3.60        # Fitting param
-    av              = 0.04#0.1        # Fitting param
+    uf              = 3.23#3.60        # Fitting param
+    av              = 0.03#0.1        # Fitting param
     
     slope           = 0.0#-0.0008205
 
@@ -289,7 +289,7 @@ def main():
 
     Par             = [nh_bp,max_f,uf,av,v_X,nh_X] # Free parameters
     Const           = [W,l,LyA,BetaPicRV,sigma_kernel,dp,v_ism,nh_ism,b_ism,T_ism,v_bp,b_bp,T_bp,b_X,T_X,slope] 
-    step            = np.array([0.1,3e-12,0.03,.01,10,0.1])/3.  # MCMC step size 0.3
+    step            = np.array([0.1,2e-12,0.03,.001,5,0.1])/3.  # MCMC step size 0.3
     #############################################
 
 
@@ -297,6 +297,7 @@ def main():
         print "Calculating the best parameters..."
         X = F,E,LyModel(Par,Const)[0]
         print "Chi2 before fit:\t",chi2(X)
+        print "Chi2red after fit:\t",chi2(X)/(len(F)-len(Par))
 
         Const[0] = l    # Since we want to plot the region where there is no data.
         f_before_fit, f_star, f_abs_ism, f_abs_bp, f_abs_X         = LyModel(Par,Const)
@@ -305,7 +306,7 @@ def main():
         RVo = wave2RV(Wo,LyA,BetaPicRV)
 
         # Plot starting point
-        fig = plt.figure(figsize=(8,6))
+        fig = plt.figure(figsize=(7,5))
         fontlabel_size  = 18
         tick_size       = 18
         params = {'backend': 'wxAgg', 'lines.markersize' : 2, 'axes.labelsize': fontlabel_size, 'font.size': fontlabel_size, 'legend.fontsize': fontlabel_size, 'xtick.labelsize': tick_size, 'ytick.labelsize': tick_size, 'text.usetex': True}
@@ -350,16 +351,19 @@ def main():
 
         X = F,E,LyModel(P,Const)[0]
         print "Chi2 after fit:\t",chi2(X)
+        print "Chi2red after fit:\t",chi2(X)/(len(RV)-len(P))
 
         Const[0] = l    # Since we want to plot the region where there is no data.
         f_after_fit, f_star, f_abs_ism, f_abs_bp, f_abs_X         = LyModel(P,Const)
 
         bin_pnts = 7
         RVb, Fb, Eb     = Bin_data(RV,F,E,bin_pnts)
-        RVob, Fob, Eob     = Bin_data(RVo,Fo,Eo,bin_pnts)
+        RVob, Fob, Eob  = Bin_data(RVo,Fo,Eo,bin_pnts)
 
         plt.scatter(RVob,Fob,color='black',alpha=0.25,label='Data not used for fit')
-        plt.errorbar(RVb,Fb,yerr=Eb,color='black',label='Data used for fit')
+
+        plt.errorbar(RVb,Fb,yerr=Eb,fmt=None,ecolor='black',zorder=3)
+        plt.scatter(RVb,Fb, marker='o', color='k',zorder=3,label='Data used for fit')
         
         '''
         for i in range(len(Wo)):
@@ -375,8 +379,8 @@ def main():
         plt.xlabel(r'Radial Velocity [km/s]')
         plt.ylabel('Flux (erg/s/cm$^2$/\AA)')
 
-        plt.xlim(-700,600)
-        plt.ylim(-2.0e-14,0.4e-13)
+        plt.xlim(-710,600)
+        plt.ylim(-2.2e-14,7.3e-14)
         fig.tight_layout()
         plt.show()
 
@@ -389,7 +393,7 @@ def main():
    
         chain, moves = MCMC(W,X,LyModel,Par,Const,step,1e5)
         
-        outfile = 'chain_c_4'
+        outfile = '../chains/chain_c_4'
         np.savez(outfile, nh_bp = chain[:,0], max_f = chain[:,1], uf = chain[:,2], av = chain[:,3], v_X = chain[:,4], nh_X = chain[:,5])
         
         Pout = chain[moves,:]
